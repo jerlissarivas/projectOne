15d0c6851f97a2caf65715d443a2875ba16d10d3
Jerlissa Rivas <jerlissarivas@Jerlissas-MacBook-Air.local>
2019-12-04 21:23:21 -0500

    Add 4 more bricks

--- js/index.js
+++ js/index.js
@@ -13,7 +13,11 @@ window.addEventListener("load", () => {
         }
     }
 
-    drawImg("mainChar", "./images/Mario_NSMB2.png", 0, 325, 65, 65)
-    drawImg("brick", "./images/cartoon-brick-wall-6.png", 10, 525, 200, 200)
+    drawImg("mainChar", "./images/Mario_NSMB2.png", 0, 525, 65, 65)
+    drawImg("brick", "./images/cartoon-brick-wall-6.png", 150, 375, 75, 75)
+    drawImg("brick", "./images/cartoon-brick-wall-6.png", 205, 375, 75, 75)
+    drawImg("brick", "./images/cartoon-brick-wall-6.png", 260, 375, 75, 75)
+    drawImg("brick", "./images/cartoon-brick-wall-6.png", 315, 375, 75, 75)
+    drawImg("brick", "./images/cartoon-brick-wall-6.png", 370, 375, 75, 75)
 
 })
