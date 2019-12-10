7a5f55e684224af2833eaccde09ebd26eed732a4
Jerlissa Rivas <jerlissarivas@Jerlissas-MacBook-Air.local>
2019-12-09 21:25:08 -0500

    Added obstacle movement and score addition. Error with both features. Work on debugging

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ const ctx = myCanvas.getContext('2d');
 myCanvas.width = 1400;
 myCanvas.height = 650;
 
+let score = 0;
+let isOver = false;
+
 const mainChar = new Image();
 mainChar.src = './images/Mario_NSMB2.png';
 let mainCharX = 0;
@@ -10,11 +13,10 @@ let mainCharY = 525;
 
 const obstacleImg = new Image();
 obstacleImg.src = './images/bomb-clipart.png';
-let obstacleX = 1000;
+let obstacleX = 1340;
 let obstacleY = 545;
 
 function drawImg(name, pathToImg, x, y, w, h) {
-    console.log("drawwwwwwww=-=-=-=-=-=-=-= ", name)
     name = new Image();
     name.src = pathToImg;
     // name.onload = () => {
@@ -22,34 +24,33 @@ function drawImg(name, pathToImg, x, y, w, h) {
     // };
 }
 function drawEverything() {
-    console.log("called drawingeverything")
     drawImg(mainChar, "./images/Mario_NSMB2.png", mainCharX, mainCharY, 65, 65);
     drawImg(obstacleImg, "./images/bomb-clipart.png", obstacleX, obstacleY, 60, 60);
-    console.log("hello!!!!!!")
-    // if (checkCollision(mainCharX, mainCharY, obstacleX, obstacleY)) {
-        //     console.log('CRASH!!!!!!');
-        //     gameOver();
-        // }
-        // if (obstacleX === 0) {
-            //     score++;
-            // }
-            drawImg('brick', './images/cartoon-brick-wall-6.png', 150, 375, 75, 75);
-            drawImg('brick', './images/cartoon-brick-wall-6.png', 205, 375, 75, 75);
-            drawImg('brick', './images/cartoon-brick-wall-6.png', 260, 375, 75, 75);
-            drawImg('brick', './images/cartoon-brick-wall-6.png', 315, 375, 75, 75);
-            drawImg('brick', './images/cartoon-brick-wall-6.png', 370, 375, 75, 75);
+    drawImg('brick', './images/cartoon-brick-wall-6.png', 150, 375, 75, 75);
+    drawImg('brick', './images/cartoon-brick-wall-6.png', 205, 375, 75, 75);
+    drawImg('brick', './images/cartoon-brick-wall-6.png', 260, 375, 75, 75);
+    drawImg('brick', './images/cartoon-brick-wall-6.png', 315, 375, 75, 75);
+    drawImg('brick', './images/cartoon-brick-wall-6.png', 370, 375, 75, 75);
+
+    if (checkCollision(mainCharX, mainCharY, obstacleX, obstacleY)) {
+            console.log('CRASH!!!!!!');
+            gameOver();
+        }
+        if (obstacleX === 0) {
+                score++;
+                console.log(score)
+            }
         }
         
 function drawingLoop() {
     ctx.clearRect(0, 0, 1400, 650);
 
-    // // drawBackground();
-    // obstacleX -= 5;
+    obstacleX -= 5;
    
-    // if (obstacleX < -70) {
-    //     obstacleX = 1000;
-    //     obstacleY = Math.floor(Math.random() * 430);
-    // }
+    if (obstacleX < -70) {
+        obstacleX = 1340;
+        // obstacleY = Math.floor(Math.random() * 430);
+    }
     drawEverything();
     // requestAnimationFrame(() => drawingLoop());
     // if (isOver === false) {
@@ -85,32 +86,15 @@ document.onkeydown = function (event) {
     }
 };
 
-// move() {
-//     document.onkeydown = event => {
-//       //   console.log("event: ", event.keyCode);
-//       const key = event.keyCode;
-//       const possibleKeystrokes = [37, 65, 38, 87, 39, 83, 40, 68];
-//       if (possibleKeystrokes.includes(key)) {
-//         event.preventDefault();
-//         switch (key) {
-//           case 37:
-//           case 65:
-//             if (this.x >= 0) this.x -= 20;
-//             break;
-//           case 38:
-//           case 87:
-//             if (this.y >= 10) this.y -= 20;
-//             break;
-//           case 39:
-//           case 83:
-//             if (this.x <= 1000 - this.width) this.x += 20;
-//             break;
-//           case 40:
-//           case 68:
-//             if (this.y <= 500 - this.height) this.y += 20;
-//             break;
-//         }
-//       }
-//     };
+function checkCollision(mainCharX, mainCharY, obstacleX, obstacleY){
+    // supermanY + superman-height >= fireballY
+return mainCharY >= obstacleY + 25
+    // supermanY <= fireballY + fireball-height
+    // && mainCharY <= obstacleY
+    // // supermanX + superman-width >= fireballX
+    // && mainCharX >= obstacleX
+    // // supermanX <= fireballX + fireball-width
+    // && mainCharX <= obstacleX;
+};
 
 drawingLoop();
