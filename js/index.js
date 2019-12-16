469180622a493d6a93f0d6454167fb895b1cc958
Jerlissa Rivas <jerlissarivas@Jerlissas-MacBook-Air.local>
2019-12-16 12:48:34 -0500

    Add checkContact function to Bricks and Coins

--- js/index.js
+++ js/index.js
@@ -12,45 +12,42 @@ let mainCharX = 0;
 let mainCharY = 525;
 
 const obstacleImg = new Image();
-obstacleImg.src = './images/Goomba.png';
+obstacleImg.src = './images/bomb-clipart.png';
 let obstacleX = 1340;
-let obstacleY = Math.floor(Math.random() * 545);
+let obstacleY = 545;
 
-const brick = new Image();
-brick.src = './images/cartoon-brick-wall-6.png';
-let brickX = 1340;
-let brickY = Math.floor(Math.random() * 545);
+// const brickImg = new Image();
+// brickImg.src = './images/cartoon-brick-wall-6.png';
+// let brickX = 1340;
+// let brickY = Math.floor(Math.random() * 600);
 
-const coin = new Image();
-coin.src = './images/bitcoin.png';
-let coinX = 1340;
-let coinY = Math.floor(Math.random() * 545);
+// const coinImg = new Image();
+// coinImg.src = './images/bitcoin.png';
+// let coinX = 1340;
+// let coinY = Math.floor(Math.random() * 600);
 
 function drawImg(name, pathToImg, x, y, w, h) {
     name = new Image();
     name.src = pathToImg;
-    // name.onload = () => {
         ctx.drawImage(name, x, y, w, h);
-    // };
 }
 function drawEverything() {
     drawImg(mainChar, "./images/Mario_NSMB2.png", mainCharX, mainCharY, 65, 65);
-    drawImg(obstacleImg, "./images/Goomba.png", obstacleX, obstacleY, 55, 55);
-    drawImg(brick, "./images/cartoon-brick-wall-6.png", brickX, brickY, 75, 75);
-    drawImg(coin, "./images/bitcoin.png", coinX, coinY, 60, 60);
+    drawImg(obstacleImg, "./images/bomb-clipart.png", obstacleX, obstacleY, 55, 55);
+    // drawImg(brickImg, "./images/cartoon-brick-wall-6.png", brickX, brickY, 75, 75);
+    // drawImg(coinImg, "./images/bitcoin.png", coinX, coinY, 60, 60);
 
-    // drawImg('brick', './images/cartoon-brick-wall-6.png', 150, 375, 75, 75);
-    // drawImg('coin', "./images/bitcoin.png", 217, 385, 60, 60);
-    // drawImg('brick', './images/cartoon-brick-wall-6.png', 275, 375, 75, 75);
-    // drawImg('coin', "./images/bitcoin.png", 440, 385, 60, 60);
-    // drawImg('coin', "./images/bitcoin.png", 500, 385, 60, 60);
-    // drawImg('coin', "./images/bitcoin.png", 560, 385, 60, 60);
-    // drawImg('brick', './images/cartoon-brick-wall-6.png', 700, 375, 75, 75);
-    // drawImg('brick', './images/cartoon-brick-wall-6.png', 755, 375, 75, 75);
-    // drawImg('coin', "./images/bitcoin.png", 822, 385, 60, 60);
-    // drawImg('brick', './images/cartoon-brick-wall-6.png', 1000, 375, 75, 75);
-    // drawImg('brick', './images/cartoon-brick-wall-6.png', 1055, 375, 75, 75);
-    // drawImg('brick', './images/cartoon-brick-wall-6.png', 755, 375, 75, 75);
+    drawImg('brick', './images/cartoon-brick-wall-6.png', 150, 375, 75, 75);
+    drawImg('coin', "./images/bitcoin.png", 217, 385, 60, 60);
+    drawImg('brick', './images/cartoon-brick-wall-6.png', 275, 375, 75, 75);
+    drawImg('coin', "./images/bitcoin.png", 440, 385, 60, 60);
+    drawImg('coin', "./images/bitcoin.png", 500, 385, 60, 60);
+    drawImg('coin', "./images/bitcoin.png", 560, 385, 60, 60);
+    drawImg('brick', './images/cartoon-brick-wall-6.png', 700, 375, 75, 75);
+    drawImg('brick', './images/cartoon-brick-wall-6.png', 755, 375, 75, 75);
+    drawImg('coin', "./images/bitcoin.png", 822, 385, 60, 60);
+    drawImg('brick', './images/cartoon-brick-wall-6.png', 1000, 375, 75, 75);
+    drawImg('brick', './images/cartoon-brick-wall-6.png', 1055, 375, 75, 75);
     
     
     
@@ -64,10 +61,66 @@ function drawEverything() {
     //     // gameOver();
     // }
 
-    if (checkCollision(mainCharX, mainCharY, coinX, coinY)) {
-        // alert("GAME OVER!");
-        score--;
-        document.getElementById('score').innerHTML = "Score: "+ score;
+    // if (checkCollision(mainCharX, mainCharY, coinX, coinY)) {
+    //     // alert("GAME OVER!");
+    //     // return score++;
+    //     document.getElementById('score').innerHTML = "Score: "+ score++;
+    // }
+
+    if (checkContact(mainCharY, 375, mainCharX, 150)) {
+        // alert("contact");
+        // return score++;
+        document.getElementById('score').innerHTML = "Score: "+ score++;
+    }
+    if (checkContact(mainCharY, 385, mainCharX, 217)) {
+        // alert("contact");
+        // return score++;
+        document.getElementById('score').innerHTML = "Score: "+ score++;
+    }
+    if (checkContact(mainCharY, 375, mainCharX, 275)) {
+        // alert("contact");
+        // return score++;
+        document.getElementById('score').innerHTML = "Score: "+ score++;
+    }
+    if (checkContact(mainCharY, 385, mainCharX, 440)) {
+        // alert("contact");
+        // return score++;
+        document.getElementById('score').innerHTML = "Score: "+ score++;
+    }
+    if (checkContact(mainCharY, 385, mainCharX, 500)) {
+        // alert("contact");
+        // return score++;
+        document.getElementById('score').innerHTML = "Score: "+ score++;
+    }
+    if (checkContact(mainCharY, 385, mainCharX, 560)) {
+        // alert("contact");
+        // return score++;
+        document.getElementById('score').innerHTML = "Score: "+ score++;
+    }
+    if (checkContact(mainCharY, 375, mainCharX, 700)) {
+        // alert("contact");
+        // return score++;
+        document.getElementById('score').innerHTML = "Score: "+ score++;
+    }
+    if (checkContact(mainCharY, 375, mainCharX, 755)) {
+        // alert("contact");
+        // return score++;
+        document.getElementById('score').innerHTML = "Score: "+ score++;
+    }
+    if (checkContact(mainCharY, 385, mainCharX, 822)) {
+        // alert("contact");
+        // return score++;
+        document.getElementById('score').innerHTML = "Score: "+ score++;
+    }
+    if (checkContact(mainCharY, 375, mainCharX, 1000)) {
+        // alert("contact");
+        // return score++;
+        document.getElementById('score').innerHTML = "Score: "+ score++;
+    }
+    if (checkContact(mainCharY, 375, mainCharX, 1055)) {
+        // alert("contact");
+        // return score++;
+        document.getElementById('score').innerHTML = "Score: "+ score++;
     }
 
     // if (obstacleX === 0){
@@ -80,34 +133,17 @@ function drawEverything() {
     //     document.getElementById('score').innerHTML = "Score: "+ score;
     // }
 }
-// let marioBottom = mainCharY + 65
-// let marioRight = mainCharX + 65
-
-    // if (checkContact(mainCharX, mainCharY)) {
-    //     alert("YOU WON!");
-    //     }
         
 function drawingLoop() {
     ctx.clearRect(0, 0, 1400, 650);
 
     obstacleX -= 5;
-    coinX -= 6;
-    brickX -= 7;
    
     if (obstacleX < -70) {
         obstacleX = 1340;
-        obstacleY = Math.floor(Math.random() * 545);
+        // obstacleY = Math.floor(Math.random() * 600);
     }
 
-    if (coinX < -70) {
-        coinX = 1340;
-        coinY = Math.floor(Math.random() * 545);
-    }
-
-    if (brickX < -70) {
-        brickX = 1340;
-        brickY = Math.floor(Math.random() * 545);
-    }
 
     drawEverything();
     // requestAnimationFrame(() => drawingLoop());
@@ -129,7 +165,7 @@ document.onkeydown = function (event) {
             break;
         case 38: // UP
         case 87:
-            if (mainCharY >= 10) mainCharY -= 25;
+            if (mainCharY >= 450) mainCharY -= 25;
             break;
         case 39: // RIGHT
         case 83:
@@ -156,16 +192,16 @@ document.onkeydown = function (event) {
 // }
 
 
-// function checkContact(obj1x, obj1y, obj2x, obj2y){
-//     // mainCharY + mainChar-height >= obstacleY
-// return obj1x + 65 - 10 <= obj2x
+function checkContact(mainCharY, obstacleBottom, mainCharX, obstacleLeft){
+    // mainCharY + mainChar-height >= obstacleY
+return mainCharY <= obstacleBottom + 50
 // // mainCharY <= obstacleY + obstacle-height
-// && obj1x >= obj2x + 60
+&& mainCharX + obstacleLeft >= obstacleLeft + obstacleLeft
 // // mainCharX + mainChar-width >= obstacleX
-// && obj1y <= obj2y + 75
+&& mainCharX + obstacleLeft <= obstacleLeft + obstacleLeft + 20
 // // mainCharX <= obstacleX + obstacle-width
 // && obj1y + 65 >= obj2y;
-// };
+};
   
 
 function checkCollision(obj1x, obj1y, obj2x, obj2y){
